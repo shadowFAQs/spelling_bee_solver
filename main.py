0d2fbac26af5fd31d34b3c8602494821676e7a16
Andrew Lawler <alawler6@gmail.com>
2021-01-04 10:56:33 -0500

    Docstrings and syntax

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+"""
+Solution finder for the New York Times' Spelling Bee puzzles
+
+To use:
+
+    1. From a CLI, run main.py
+    2. When prompted, input the letters in the puzzle in any order
+    3. When prompted, specify which letter is required
+    4. spelling_bee_solver will generate and score a word list for you
+"""
 def load_dictionary():
-    # Read dictionary file and return words with at least 5 letters
-    with open('dictionary.txt') as f:
-        data = f.read().split('\n')
+    """Read dictionary file and return words with at least 5 letters"""
+    with open('dictionary.txt') as file:
+        data = file.read().split('\n')
     return [word for word in data if len(word) >= 5]
 
-def search_dictionary(d, letters, primary):
+def search_dictionary(dictionary, letters, primary):
+    """Return all matching words from dictionary"""
     # Words must contain primary letter
-    words = [w for w in d if primary in w]
+    words = [w for w in dictionary if primary in w]
 
     # Words must not contain any nonspecified letters
     return [w for w in words if all([l in letters for l in w])]
 
 def main():
-    # Load dictionary
+    """Get user input; look up matching words"""
     dictionary = load_dictionary()
 
     # Get letters
@@ -29,7 +40,8 @@ def main():
     # Find word list
     print(f'Searching for words using the letters {letters};')
     print(f'    (Must include primary letter "{primary}")...')
-    words = search_dictionary(d=dictionary, letters=letters, primary=primary)
+    words = search_dictionary(
+        dictionary=dictionary, letters=letters, primary=primary)
 
     # Display found words
     points = 0
